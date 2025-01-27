0a3b6aedae357b8dc2a0042c8248282a364b003f
oldmanshavingcode <168641238+oldmanshavingcode@users.noreply.github.com>
2025-01-27 23:38:16 +0900

    Update kakaotalkbot_myself.py
    내 프로필 가져오기 삭제(불필요)

--- kakaotalkbot_myself.py
+++ kakaotalkbot_myself.py
@@ -32,21 +32,7 @@ def set_token(script_path):
 def convert_notice_to_text(notice):
     return f"[{notice['source']}] {notice['title']}"
 
-'''
-# 2. 내 프로필 정보(이름) 가져오기(옵션 : 카카오톡 api 디버깅용)
-def get_my_profile():
-    with open(r"for_kakao_real\kakao_code.json", "r") as fp:
-        tokens = json.load(fp)
-
-    url = "https://kapi.kakao.com/v1/api/talk/profile"  # 내 프로필 정보 가져오기
-    header = {"Authorization": 'Bearer ' + tokens["access_token"]}
-    result = json.loads(requests.get(url, headers=header).text)
-    print(result)
-    my_profile = result.get("nickName")
-    print(my_profile)
-'''
-
-# 3. 내게 쓰기
+# 2. 내게 쓰기
 def send_myself(message_text, link):
     with open(r"for_kakao\kakao_code.json", "r") as fp:
         tokens = json.load(fp)
@@ -79,18 +65,13 @@ def main():
     # 1. 크롤러 실행후 새로 갱신된 공지사항 추출 (새로운 공지사항이 있으면 crawl.txt에 기록)
     mod_vars = runpy.run_path("basic_webcrawler.py")
 
-    '''
-    # 2. 카카오톡 내 프로필 가져오기(옵션)
-    get_my_profile()
-    '''
-
-    # 3. all_new_notices 추출
+    # 2. all_new_notices 추출
     all_new_notices = mod_vars.get("all_new_notices", [])
     if not all_new_notices:
         print("새로운 공지사항이 없습니다.")
         return
 
-    # 4. 각 공지사항을 개별적으로 전송
+    # 3. 각 공지사항을 개별적으로 전송
     for notice in all_new_notices:
         message_text = convert_notice_to_text(notice)
         link = notice['link']
